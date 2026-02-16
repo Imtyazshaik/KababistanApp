a834dbe9782e6f00245b1f104eeeb3fbc8ff3c71
sabdul-7 <imtyazshaik05@gmail.com>
2026-02-15 21:34:58 -0800

    Correction

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kababistan App"
+include(":app")
+include(":admin")
